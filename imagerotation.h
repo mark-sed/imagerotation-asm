2244b84e53a3963a0911aa22a8ea92f52e76867b
Marek Sedlacek <mr.mareksedlacek@gmail.com>
2020-05-01 22:05:33 +0200

    Added documentation

--- imagerotation.h
+++ imagerotation.h
@@ -1,14 +1,27 @@
+/**
+ * @file imagerotation.h
+ * @author Marek Sedláček
+ * @date February 2020
+ * 
+ * @brief Header file for imagerotation module
+ * 
+ * This code was made for my bachelor's thesis at
+ * Brno University of Technology
+ */
+
 #ifndef _IMAGEROTATION_H_
 #define _IMAGEROTATION_H_
 
 #include <stdint.h> 
-typedef struct{
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
-    uint8_t a;
-} TPixel;
 
+/**
+ * Function for monochromatic image rotation by arbitrary angle
+ * @param src source image
+ * @param dst image to which will be rotated image saved
+ * @param width width of the image
+ * @param height height of the image
+ * @param angle angle in radians by which the image will be rotated 
+ */
 void rotate_mc_image(uint8_t **src, uint8_t **dst, unsigned int width, unsigned int height, float angle);
 
 #endif //_IMAGEROTATION_H_
--- main.c
+++ main.c
@@ -1,16 +1,29 @@
+/**
+ * @file main.c
+ * @author Marek Sedláček
+ * @date February 2020
+ * 
+ * @brief Program for testing performance of image rotation algorithm
+ * 
+ * This code was made for my bachelor's thesis at
+ * Brno University of Technology
+ */
+
 #include "imagerotation.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 
 #ifndef M_PI
+// PI (Taken from boost library)
+// https://www.boost.org/doc/libs/1_56_0/boost/math/constants/constants.hpp
 #define M_PI 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651e+00
 #endif
 #define DEG2RAD(deg) ((deg) * M_PI / 180.0)
 
 int main(int argc, char *argv[]){
-#define WIDTH 12800
-#define HEIGHT 12800
+#define WIDTH 3840
+#define HEIGHT 2160
     uint8_t **img;
     uint8_t **dimg;
     img = malloc(WIDTH * sizeof(uint8_t *));
@@ -20,31 +33,7 @@ int main(int argc, char *argv[]){
         dimg[i] = malloc(WIDTH * sizeof(uint8_t));
     }
 
-
-    /*for(int x = 0; x < WIDTH/2; x++){
-        for(int y = 0; y < HEIGHT; y++){
-            img[x][y] = 42;
-        }
-    }*/
-
-    rotate_mc_image(img, dimg, WIDTH, HEIGHT, DEG2RAD(63));
-    /*
-    for(int x = 0; x < WIDTH; x++){
-        for(int y = 0; y < HEIGHT; y++){
-            printf("%c ", img[x][y] ? 'o' : '_');
-        }
-        printf("\n");
-    }
-
-
-    printf("\n");
-
-    for(int x = 0; x < WIDTH; x++){
-        for(int y = 0; y < HEIGHT; y++){
-            printf("%c ", dimg[x][y] ? 'o' : '_');
-        }
-        printf("\n");
-    }*/
+    rotate_mc_image(img, dimg, WIDTH, HEIGHT, DEG2RAD(42));
 
     for(long i = 0; i < WIDTH; i++){
         free(img[i]);
